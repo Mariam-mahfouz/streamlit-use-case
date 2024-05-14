124ee03f132d2df4a171f270be3db6bab73e9a4e
Sulaiman-Alharbi <Sulaiman.falharbi@gmail.com>
2024-05-14 13:24:42 +0300

    Lucid1

--- app.py
+++ app.py
@@ -6,6 +6,4 @@ df = pd.read_csv('Data\clean.csv')
 
 st.title('Find Your Perfect Used Car – Great Prices and Financing Options!')
 st.markdown("*Streamlit* is **really** ***cool***.")
-st.markdown('''
-    :red[Streamlit] :orange[can] :green[write] :blue[text] :violet[in]
-    :gray[pretty] :rainbow[colors] and :blue-background[highlight] text.''')
+
