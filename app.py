faeb26ad476c058d10e61a64916a53df2784a911
Sulaiman-Alharbi <Sulaiman.falharbi@gmail.com>
2024-05-14 13:24:11 +0300

    Lucid

--- app.py
+++ app.py
@@ -2,4 +2,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in NYC')
+df = pd.read_csv('Data\clean.csv')
+
+st.title('Find Your Perfect Used Car – Great Prices and Financing Options!')
+st.markdown("*Streamlit* is **really** ***cool***.")
+st.markdown('''
+    :red[Streamlit] :orange[can] :green[write] :blue[text] :violet[in]
+    :gray[pretty] :rainbow[colors] and :blue-background[highlight] text.''')
